7a6c5a03765f284f26430600cce7c450f6efc7ba
Andres Romero <aromerosambucetti@gmail.com>
2023-11-28 21:52:34 -0300

    entrega

--- src/productManager.js
+++ src/productManager.js
@@ -8,7 +8,7 @@ class ProductManager {
         this.products = [];
         this.readProducts();
     }
-
+// Función para cargar los datos del JSON
     async readProducts() {
         try {
             const data = await fs.promises.readFile(this.path, 'utf8');
@@ -18,14 +18,14 @@ class ProductManager {
             throw new Error(`No se encontró ${this.path}: ${error.message}`);
         }
     }
-
+ // Función para generar el ID
     updateLastId() {
         if (this.products.length > 0) {
           const maxId = Math.max(...this.products.map(product => product.id));
           this.contador = maxId + 1;
         }
     }
-
+// Función para guardar los datos en el JSON
     async writeProducts() {
         try {
             await fs.promises.writeFile(this.path, JSON.stringify(this.products, null, 2), 'utf8');
@@ -33,7 +33,7 @@ class ProductManager {
             throw new Error(`Error al escribir en ${this.path}: ${error.message}`);
         }
     }      
-
+  // Función para crear un producto
     async addProduct(product){
         if (!product.title || !product.description || !product.price || !product.thumbnail || !product.code ||  !product.stock){
             throw new Error(`Debe ingresar todos los campos`);
@@ -47,15 +47,15 @@ class ProductManager {
         this.products.push(product);
         await this.writeProducts();
     }
-
+    // Función para ver un producto y de forma limitada
     async getProductsLimit(limit) {
         return limit ? this.products.slice(0, limit) : this.products;
     }
-
+    // Función para ver los productos
     async getProducts(){
         return this.products;
     }
-
+    // Función para ver producto por id
     async getProductById(id){
         const findProduct = this.products.find((producto) => producto.id === id);
         if (!findProduct){
@@ -63,7 +63,7 @@ class ProductManager {
         }
         return findProduct;  
     }
-    
+    // Función para modificar un producto
     async updateProduct(id, updatedFields) {
         const productIndex = this.products.findIndex((producto) => producto.id === id);
 
@@ -77,7 +77,7 @@ class ProductManager {
         console.log(`El producto con el ID ${id} se actualizó correctamente`);
         await this.writeProducts();
     }
-
+    // Función para borrar un producto
     async deleteProduct(id){
         const productIndex = this.products.findIndex((producto) => producto.id === id);
         if (productIndex === -1){
