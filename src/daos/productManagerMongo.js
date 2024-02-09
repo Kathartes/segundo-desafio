ff800f5f0f3e89dfed3185864c9693f908bb914a
Andres Romero <aromerosambucetti@gmail.com>
2024-02-08 21:33:42 -0300

    entrega

--- src/daos/productManagerMongo.js
+++ src/daos/productManagerMongo.js
@@ -8,22 +8,16 @@ class ProductDaoMongo {
     async addProduct(product) {
         return await this.model.create(product);
     }
-
-    //Muestra todos los productos 
     async getProducts() {
         return await this.model.find();
     }
-
     async getProductsLimited({ filter, options }) {
         return await this.model.paginate(filter, options);
     } 
-
-    //Mostrar un producto segun ID
     async getProductById(id) { 
         return await this.model.findById(id);
     }
 
-    //Actualizar un producto segun ID
     async updateProduct(pid, updatedFields) {
         return await this.model.findOneAndUpdate(
             { _id: pid },
@@ -32,7 +26,6 @@ class ProductDaoMongo {
         );
     }
 
-    //Eliminar producto segun ID
     async deleteProduct(pid) { 
         return await this.model.findByIdAndDelete(pid);
     }
