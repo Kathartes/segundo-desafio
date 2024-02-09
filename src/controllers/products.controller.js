aa8e6b1c09d7c4d0f161ddf85b7bbacc4c2f3e0b
Andres Romero <aromerosambucetti@gmail.com>
2024-02-08 21:33:09 -0300

    entrega

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -4,27 +4,123 @@ class ProductController{
     constructor(){
         this.productService = new productDaoMongo()
     }
-    getProducts = async(req,res)=>{ 
-        const result = await this.productService.getProduct();
-        res.json({result})
+    async getProductsLimited(req, res) {
+        try {
+            const { limit = 3, page = 1, sort, query } = req.query;
+    
+            const options = {
+                page: parseInt(page),
+                limit: parseInt(limit),
+                sort: sort ? { price: sort === 'asc' ? 1 : -1 } : undefined,
+            };
+    
+            let filter = {};
+    
+            if (query) {
+                const isStatus = query.toLowerCase() === 'true' || query.toLowerCase() === 'false';
+    
+                if (isStatus) {
+                    filter.status = query.toLowerCase() === 'true';
+                } else {
+                    filter.category = query;
+                }
+            }
+    
+            const result = await this.productService.getProductsLimited({ filter, options });
+    
+            const response = {
+                status: 'success',
+                payload: result.docs,
+                totalPages: result.totalPages,
+                prevPage: result.prevPage,
+                nextPage: result.nextPage,
+                page: result.page,
+                hasPrevPage: result.hasPrevPage,
+                hasNextPage: result.hasNextPage,
+                prevLink: result.hasPrevPage ? `/products?limit=${limit}&page=${result.prevPage}` : null,
+                nextLink: result.hasNextPage ? `/products?limit=${limit}&page=${result.nextPage}` : null,
+            };
+    
+            res.json(response);
+        } catch (error) {
+            console.error(error.message);
+            res.status(500).send('Internal Server Error');
+        }
     }
-    getProductByFilter = async(req,res)=>{
-        const { name } = req.params;
-        const productFound = await this.productService.getProductByFilter(name);
-        res.json({productFound})
+    
+
+
+    getProductById = async (req, res) => {
+        try {
+            const productId = req.params.pid;
+            const product = await this.productService.getProductById(productId);
+            res.json({ product });//res.send({status: 'success', payload: product})
+        } catch (error) {
+            console.error(error.message);
+            res.status(404).send('Product Not Found');
+        }
     }
-    createProduct = async(req,res)=> {
-        const newProduct = await this.productService.createProduct(req.body);
-        res.json({newProduct})
+
+    addProduct = async (req, res) => {
+        try {
+            // Realiza las validaciones y manejo de errores aquí antes de llamar a addProduct en el service.
+            const { title, description, price, code, stock, category, thumbnails } = req.body;
+    
+            if (!title || !description || !price || !code || !stock || !category) {
+                console.error('Todos los campos son obligatorios.');
+                return res.status(400).send('Bad Request');
+            }
+    
+            const existingProduct = await this.productService.checkExistingProduct(code);
+    
+            if (existingProduct) {
+                console.error('El campo "code" ya está en uso.');
+                return res.status(400).send('Bad Request');
+            }
+    
+            const newProduct = await this.productService.addProduct({ 
+                title, 
+                description, 
+                price, 
+                code, 
+                stock, 
+                category,
+                thumbnails });
+    
+            res.json({ product: newProduct });
+        } catch (error) {
+            console.error(error.message);
+            res.status(400).send('Bad Request');
+        }
     }
-    updateProduct = async(req,res)=>{
-        const productId = req.params.pid;
-        const updatedProduct = await this.productService.updateProduct(productId, req.body);
-        res.json({updatedProduct})
+    
+
+    updateProduct = async (req, res) => {
+        try {
+            const productId = req.params.pid;
+            const updatedProduct = await this.productService.updateProduct(productId, req.body);
+            res.json({ product: updatedProduct });//res.send({status: 'success', payload: updatedProduct})
+        } catch (error) {
+            console.error(error.message);
+            res.status(404).send('Product Not Found');
+        }
     }
-    deleteProduct = async(req,res)=>{
-        const productId = req.params.pid;
-        const deletedProduct = await this.productService.deleteProduct(productId);
-        res.json({deletedProduct})
+
+    deleteProduct = async (req, res) => {
+        try {
+            const productId = req.params.pid;
+            const deletedProduct = await this.productService.deleteProduct(productId);
+
+            if (deletedProduct) {
+                res.json({ product: deletedProduct });//res.send({status: 'success', payload: deletedProduct})
+            } else {
+                res.status(404).send('Product Not Found');
+            }
+        } catch (error) {
+            console.error(error.message);
+            res.status(500).send('Internal Server Error');
+        }
     }
 }
+
+module.exports = ProductController
--- src/daos/productManagerMongo.js
+++ src/daos/productManagerMongo.js
@@ -6,105 +6,36 @@ class ProductDaoMongo {
     }
 
     async addProduct(product) {
-        try {
-            if (!product.title || !product.description || !product.price || !product.code || !product.stock || !product.category) {
-                console.error('Todos los campos son obligatorios.');
-                return null;
-            }
-
-            const existingProduct = await this.model.findOne({ code: product.code });
-            if (existingProduct) {
-                console.error('El campo "code" ya está en uso.');
-                return null;
-            }
-
-        
-            const thumbnails = product.thumbnails || [];
-
-
-          
-            const newProduct = await this.model.create({
-                title: product.title,
-                description: product.description,
-                price: product.price,
-                code: product.code,
-                stock: product.stock,
-                category: product.category,
-                status: true,
-                thumbnails: thumbnails,
-            });
-            console.log('Created Product:', newProduct);
-            return newProduct;
-        } catch (error) {
-            console.error('Error al agregar el producto:', error.message);
-            throw error;
-        }
+        return await this.model.create(product);
     }
 
-
+    //Muestra todos los productos 
     async getProducts() {
-        try {
-            const products= await this.model.find();
-            return products
-          } catch (error) {
-            console.error('Error al obtener los productos:', error.message);
-            throw error;
-          }
+        return await this.model.find();
     }
 
-    async getProductsLimited(limit) {
-        if (limit) {
-            return this.model.find().limit(limit);
-        } else {
-            return this.model.find();
-        }
-    }
+    async getProductsLimited({ filter, options }) {
+        return await this.model.paginate(filter, options);
+    } 
 
+    //Mostrar un producto segun ID
     async getProductById(id) { 
-        try {
-            const product = await this.model.findById(id);
-      
-            if (!product) {
-                throw new Error('El producto no fue encontrado.');
-            }
-      
-            return product;
-        } catch (error) {
-            console.error('Error al obtener el producto por ID:', error.message);
-            throw error;
-        }
+        return await this.model.findById(id);
     }
 
+    //Actualizar un producto segun ID
     async updateProduct(pid, updatedFields) {
-        try {
-            const updatedProduct = await this.model.findOneAndUpdate(
-                { _id: pid },
-                { $set: updatedFields },
-                { new: true }
-            );
-
-            if (updatedProduct) {
-                return updatedProduct;
-            } else {
-                console.error('No se encontró el producto para actualizar.');
-                return null;
-            }
-        } catch (error) {
-            console.error('Error al actualizar el producto:', error.message);
-            throw error;
-        }
+        return await this.model.findOneAndUpdate(
+            { _id: pid },
+            { $set: updatedFields },
+            { new: true }
+        );
     }
 
+    //Eliminar producto segun ID
     async deleteProduct(pid) { 
-        try {
-            const deletedProduct = await this.model.findByIdAndDelete(pid);
-            return deletedProduct;
-        } catch (error) {
-            console.error('Error al eliminar el producto:', error.message);
-            throw error;
-        }
+        return await this.model.findByIdAndDelete(pid);
     }
-
 }
 
 module.exports = ProductDaoMongo
